501a2d129d389de5f518a3097b254078bbf56d48
jpwalls08 <jpwalls08@outlook.com>
2022-10-06 19:40:44 -0500

    Deleted obj and added Controllers (i.e., Equipments, EquipmentStatuts, EquipmentTypes, GolfStores)

---
+++ StoreFront.UI.MVC/Controllers/EquipmentsController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StoreFront.DATA.EF.Models;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class EquipmentsController : Controller
+    {
+        private readonly StoreFrontContext _context;
+
+        public EquipmentsController(StoreFrontContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Equipments
+        public async Task<IActionResult> Index()
+        {
+            var storeFrontContext = _context.Equipment.Include(e => e.EquipmentType).Include(e => e.Status).Include(e => e.Store);
+            return View(await storeFrontContext.ToListAsync());
+        }
+
+        // GET: Equipments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Equipment == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipment
+                .Include(e => e.EquipmentType)
+                .Include(e => e.Status)
+                .Include(e => e.Store)
+                .FirstOrDefaultAsync(m => m.EquipmentId == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // GET: Equipments/Create
+        public IActionResult Create()
+        {
+            ViewData["EquipmentTypeId"] = new SelectList(_context.EquipmentTypes, "EquipmentTypeId", "TypeName");
+            ViewData["StatusId"] = new SelectList(_context.EquipmentStatuses, "StatusId", "StatusName");
+            ViewData["StoreId"] = new SelectList(_context.GolfStores, "StoreId", "PhoneNumber");
+            return View();
+        }
+
+        // POST: Equipments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EquipmentId,EquipmentName,EquipmentPrice,EquipmentDescription,StoreId,EquipmentTypeId,StatusId,ProductImage")] Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EquipmentTypeId"] = new SelectList(_context.EquipmentTypes, "EquipmentTypeId", "TypeName", equipment.EquipmentTypeId);
+            ViewData["StatusId"] = new SelectList(_context.EquipmentStatuses, "StatusId", "StatusName", equipment.StatusId);
+            ViewData["StoreId"] = new SelectList(_context.GolfStores, "StoreId", "PhoneNumber", equipment.StoreId);
+            return View(equipment);
+        }
+
+        // GET: Equipments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Equipment == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipment.FindAsync(id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+            ViewData["EquipmentTypeId"] = new SelectList(_context.EquipmentTypes, "EquipmentTypeId", "TypeName", equipment.EquipmentTypeId);
+            ViewData["StatusId"] = new SelectList(_context.EquipmentStatuses, "StatusId", "StatusName", equipment.StatusId);
+            ViewData["StoreId"] = new SelectList(_context.GolfStores, "StoreId", "PhoneNumber", equipment.StoreId);
+            return View(equipment);
+        }
+
+        // POST: Equipments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EquipmentId,EquipmentName,EquipmentPrice,EquipmentDescription,StoreId,EquipmentTypeId,StatusId,ProductImage")] Equipment equipment)
+        {
+            if (id != equipment.EquipmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipmentExists(equipment.EquipmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EquipmentTypeId"] = new SelectList(_context.EquipmentTypes, "EquipmentTypeId", "TypeName", equipment.EquipmentTypeId);
+            ViewData["StatusId"] = new SelectList(_context.EquipmentStatuses, "StatusId", "StatusName", equipment.StatusId);
+            ViewData["StoreId"] = new SelectList(_context.GolfStores, "StoreId", "PhoneNumber", equipment.StoreId);
+            return View(equipment);
+        }
+
+        // GET: Equipments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Equipment == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipment
+                .Include(e => e.EquipmentType)
+                .Include(e => e.Status)
+                .Include(e => e.Store)
+                .FirstOrDefaultAsync(m => m.EquipmentId == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // POST: Equipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Equipment == null)
+            {
+                return Problem("Entity set 'StoreFrontContext.Equipment'  is null.");
+            }
+            var equipment = await _context.Equipment.FindAsync(id);
+            if (equipment != null)
+            {
+                _context.Equipment.Remove(equipment);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EquipmentExists(int id)
+        {
+          return _context.Equipment.Any(e => e.EquipmentId == id);
+        }
+    }
+}
---
+++ StoreFront.UI.MVC/Controllers/GolfStoresController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StoreFront.DATA.EF.Models;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class GolfStoresController : Controller
+    {
+        private readonly StoreFrontContext _context;
+
+        public GolfStoresController(StoreFrontContext context)
+        {
+            _context = context;
+        }
+
+        // GET: GolfStores
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.GolfStores.ToListAsync());
+        }
+
+        // GET: GolfStores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.GolfStores == null)
+            {
+                return NotFound();
+            }
+
+            var golfStore = await _context.GolfStores
+                .FirstOrDefaultAsync(m => m.StoreId == id);
+            if (golfStore == null)
+            {
+                return NotFound();
+            }
+
+            return View(golfStore);
+        }
+
+        // GET: GolfStores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GolfStores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StoreId,StoreName,PhoneNumber,Region,Country,ZipCode,State,Address")] GolfStore golfStore)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(golfStore);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(golfStore);
+        }
+
+        // GET: GolfStores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.GolfStores == null)
+            {
+                return NotFound();
+            }
+
+            var golfStore = await _context.GolfStores.FindAsync(id);
+            if (golfStore == null)
+            {
+                return NotFound();
+            }
+            return View(golfStore);
+        }
+
+        // POST: GolfStores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StoreId,StoreName,PhoneNumber,Region,Country,ZipCode,State,Address")] GolfStore golfStore)
+        {
+            if (id != golfStore.StoreId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(golfStore);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GolfStoreExists(golfStore.StoreId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(golfStore);
+        }
+
+        // GET: GolfStores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.GolfStores == null)
+            {
+                return NotFound();
+            }
+
+            var golfStore = await _context.GolfStores
+                .FirstOrDefaultAsync(m => m.StoreId == id);
+            if (golfStore == null)
+            {
+                return NotFound();
+            }
+
+            return View(golfStore);
+        }
+
+        // POST: GolfStores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.GolfStores == null)
+            {
+                return Problem("Entity set 'StoreFrontContext.GolfStores'  is null.");
+            }
+            var golfStore = await _context.GolfStores.FindAsync(id);
+            if (golfStore != null)
+            {
+                _context.GolfStores.Remove(golfStore);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GolfStoreExists(int id)
+        {
+          return _context.GolfStores.Any(e => e.StoreId == id);
+        }
+    }
+}
